6470273e03321657fb345bd6c9b390b1e8a74aaa
shanein <samuelhanein@gmail.com.com>
2021-05-16 20:08:15 +0200

    ajout des operations

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -13,26 +13,97 @@ export class GameComponent implements OnInit {
   score!: number;
   maxtimetrue!: number;
 
+  nb1!: number;
+  nb2!: number;
+  ope!: string;
+
+  nb3!: number;
+  ope2!: string;
+
+  resultaffiche!: string;
+
+  result!: number;
+  resultrep!: number;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  nbrandom() {
+    return  Math.floor(Math.random() * (10 - -10 + 1)) + -10;
+  }
+
+  operandom() {
+    let tab = ['+', '-', '*', '/', '^'];
+    return tab[Math.floor(Math.random() * tab.length)];
+  }
+
+  operation(nb1:number, nb2:number, ope:string) {
+    switch (ope) {
+      case '+':
+        return (nb1 + nb2);
+      case '-':
+        return (nb1 - nb2);
+      case '*':
+        return (nb1 * nb2);
+      case '/':
+        return (nb1 / nb2);
+      case '^':
+        return (nb1 ** nb2);
+    }
+  }
+
+  question() {
+    this.nb1 = this.nbrandom();
+    this.nb2 = this.nbrandom();
+    this.ope = this.operandom();
+
+    this.nb3 = this.nbrandom();
+    this.ope2 = this.operandom();
+
+
+    if (Math.floor(Math.random() * 2) == 0) {
+      this.resultaffiche = '(' + this.nb1 + ' ' + this.ope + ' ' + this.nb2 + ')';
+      console.log(this.resultaffiche);
+      this.result = this.operation(this.nb1, this.nb2, this.ope);
+      console.log(this.result);
+    }
+
+    else {
+      this.resultaffiche = '(' + this.nb1 + ' ' + this.ope + ' ' + this.nb2 + ')'
+
+      this.resultaffiche = '(' + this.resultaffiche + ' ' + this.ope2 + ' ' + this.nb3 + ')';
+      console.log(this.resultaffiche);
+      this.result = this.operation(this.operation(this.nb1, this.nb2, this.ope), this.nb3, this.ope2);
+      console.log(this.result);
+    }
+
+    if (Math.floor(Math.random() * 2) == 0) {
+      this.resultrep = this.result;
+    }
+    else {
+      this.resultrep = (this.result*this.nb1)+this.nb2;
+    }
+  }
+
   handleClick() {
     this.gameStarted = true;
-    this.compt = 200;
+    this.compt = 2000;
     this.zero = '0';
     this.score = 0;
     this.maxtimetrue = 5;
     this.decrementCompt();
+    this.question();
   }
+
 
   verif(check: boolean) {
     //determiner si la reponse est oui ou non
     if (check == true) {
       //oui
-      if (1 == 2) {
+      if (this.result == this.resultrep) {
         //si la reponse est cencé etre correct
         this.reponse(true);
       } else {
@@ -42,7 +113,7 @@ export class GameComponent implements OnInit {
     }
     if (check == false) {
       //non
-      if (1 == 2) {
+      if (this.result ==  this.resultrep) {
         //si la reponse est cencé etre correct
         this.reponse(false);
       } else {
@@ -53,6 +124,7 @@ export class GameComponent implements OnInit {
   }
   reponse(result: boolean) {
     if (result == true) {
+      this.question();
       this.score ++;
       this.compt += this.maxtimetrue * 100;
       if (this.maxtimetrue > 3)
